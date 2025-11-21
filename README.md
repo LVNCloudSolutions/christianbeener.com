1fac384f23900e1030dd46b717d964b956db4607
LVNCloudSolutions <christian@lvncloudsolutions.com>
2025-11-21 12:37:14 -0600

    Add dynamic projects data and update README
    Init-commit v2:
    Introduced scripts/projects.js to store project information for dynamic content injection. Updated README to reflect the shift from static to dynamic frontend and clarified the use of JavaScript for content management.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# christianbeener.com - static-starter
+# christianbeener.com - dynamic-frontend
 
-## An evolving project 
+## An evolving project
 
-This version is built using static HTML, JavaScript, and tailwindCSS built during deployment. The GitHub Actions workflow is specified in the [static.yml](.github/workflows/static.yml) file.
+This version is built using static HTML, JavaScript, and tailwindCSS built during deployment. As this site grows, maintaing the content in HTML will not be ideal, so this version uses JavaScript to dynamically inject content into the page.
 
-#### \*This branch is no longer in active development.
+##### The GitHub Actions workflow is specified in the [static.yml](.github/workflows/static.yml) file.
---
+++ scripts/projects.js
@@ -0,0 +1,57 @@
+const projects = [
+	{
+		title: "IaC using Terraform",
+		description: `
+            An ongoing project to create reusable
+            <strong>
+                Infrastructure as Code (IaC) templates
+            </strong>
+            for provisioning secure and scalable AWS
+            cloud resources using
+            <strong>Terraform</strong>.
+        `,
+		techStack: "IaC, Terraform, AWS",
+		url: "https://github.com/lvncloudsolutions/terraform-projects",
+		src: "GitHub",
+		startDate: "Nov 2025",
+		endDate: null,
+		isComplete: false,
+	},
+	{
+		title: "SAA-C03 Certification",
+		description: `
+            Currently studying for the
+            <strong
+                >AWS Certified Solutions Architect -
+                Associate (SAA-C03)</strong
+            >
+            exam, focusing on designing resilient
+            architectures, high-performing systems, and
+            cost-optimized solutions on AWS.
+        `,
+		techStack: "AWS, Cloud Architecture",
+		url: "https://aws.amazon.com/certification/certified-solutions-architect-associate/",
+		src: "AWS",
+		startDate: "Nov 2025",
+		endDate: null,
+		isComplete: false,
+	},
+	{
+		title: "Launch Static Website",
+		description: `
+            This static website was built using
+            <strong>HTML5</strong>,
+            <strong>Vanilla JavaScript</strong>, and
+            <strong>Tailwind CSS</strong> via CDN. It is
+            hosted on GitHub Pages, and deployments are
+            automated through a
+            <strong>CI/CD pipeline</strong> built in
+            GitHub Actions.
+        `,
+		isComplete: true,
+		startDate: "Nov 2025",
+		endDate: "Nov 2025",
+		url: "https://github.com/lvncloudsolutions/christianbeener.com",
+		src: "GitHub",
+	},
+];
