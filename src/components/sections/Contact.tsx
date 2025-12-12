d771cfe61725fd9c6f86df36c37a1f6e687d9c0b
LVNCloudSolutions <christian@lvncloudsolutions.com>
2025-12-12 15:51:07 -0600

    Moved toast notification into ContactForm

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -2,19 +2,9 @@ import { useState } from "react";
 import TabGroup from "../general/TabGroup";
 import ContactForm from "../general/ContactForm";
 import { SERVICE_FORM, RECRUITER_FORM } from "../../utils/constants";
-import ToastNotification from "../general/ToastNotification";
 
 export default function Contact() {
 	const [activeTab, setActiveTab] = useState("services");
-	// NEW: State for Toast and Submission Status
-	const [toastStatus, setToastStatus] = useState<
-		"idle" | "success" | "error"
-	>("idle");
-	const [toastMessage, setToastMessage] = useState("");
-	const handleToastClose = () => {
-		setToastStatus("idle");
-		setToastMessage("");
-	};
 
 	return (
 		<>
@@ -29,14 +19,6 @@ export default function Contact() {
 						? (SERVICE_FORM as any)
 						: (RECRUITER_FORM as any)
 				}
-				setToastStatus={setToastStatus}
-				setToastMessage={setToastMessage}
-			/>
-
-			<ToastNotification
-				status={toastStatus}
-				message={toastMessage}
-				onClose={handleToastClose}
 			/>
 		</>
 	);
