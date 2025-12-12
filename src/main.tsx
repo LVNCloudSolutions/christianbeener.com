db4b2515f052040ccb08fe37eb30009838d0dac9
LVNCloudSolutions <christian@lvncloudsolutions.com>
2025-12-12 15:39:43 -0600

    Google Captcha
    added GoogleRecaptchaProvider

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,15 @@ import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./App.tsx";
 
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
+
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<App />
+		<GoogleReCaptchaProvider
+			reCaptchaKey={import.meta.env.VITE_RECAPTCHA_SITE_KEY}
+			useEnterprise={false}
+		>
+			<App />
+		</GoogleReCaptchaProvider>
 	</StrictMode>
 );
